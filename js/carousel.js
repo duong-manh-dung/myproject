01ffca3752798d2fbc86eb97133ef6b58d7a767b
duong-manh-dung <duongdung23@icloud.com>
2022-01-28 14:16:31 +0700

    sixth commit

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,9 @@
-$('.owl-carousel').owlCarousel({
-  margin: 10,
-  loop: true,
-  autoWidth: false,
-  items: 1,
-  autoplay: true,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true
-})
+// $('.owl-carousel').owlCarousel({
+//   margin: 10,
+//   loop: true,
+//   autoWidth: false,
+//   items: 1,
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true
+// })
